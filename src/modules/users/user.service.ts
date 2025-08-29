f615ea35ae9eb4a1b914df2e73fd2ccf470d3dc7
EderSouza93 <ederaldo.souza93@gmail.com>
2025-08-28 23:13:46 -0300

    sparkles: feat: remove password of response

--- src/modules/users/user.service.ts
+++ src/modules/users/user.service.ts
@@ -4,17 +4,24 @@ import { User } from "@prisma/client";
 import { CreateUserDTO } from "./domain/dto/createUser.dto";
 import { UpdateUserDto } from "./domain/dto/updateUser.dto";
 import * as bcrypt from 'bcrypt';
+import { userSelectFields } from "../prisma/utils/userSelectFields";
 
 @Injectable()
 export class UserService {
     constructor(private readonly prisma: PrismaService) {}
     
+    // Need to remove updateAt and password for dont show on response
     async create(body: CreateUserDTO): Promise<User> {
         body.password = await this.hashPassword(body.password);
-        return await this.prisma.user.create({data: body});
+        return await this.prisma.user.create({
+            data: body,
+            select: userSelectFields
+        });
     }
     async list() {
-        return await this.prisma.user.findMany();
+        return await this.prisma.user.findMany({
+            select: userSelectFields
+        });
     }
     async show(id: number) {
         const user = await this.isIdExists(id);  
@@ -26,16 +33,24 @@ export class UserService {
         if (body.password) {
             body.password = await this.hashPassword(body.password)
         }
-        return await this.prisma.user.update({ where: { id }, data: body });
+        return await this.prisma.user.update({ 
+            where: { id }, 
+            data: body,
+            select: userSelectFields
+        });
     }
     async delete(id: number) {
         await this.isIdExists(id);
-        return await this.prisma.user.delete({ where: { id }});
+        return await this.prisma.user.delete({ 
+            where: { id },
+            select: userSelectFields
+        });
     }
 
     private async isIdExists(id: number) {
         const user = await this.prisma.user.findUnique({
             where: { id },
+            select: userSelectFields
         });
 
         if (!user) {
