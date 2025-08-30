aaa79adcbe3078274385eeecd375985b8f681a21
EderSouza93 <ederaldo.souza93@gmail.com>
2025-08-30 12:26:20 -0300

    :sparkles: feat: implement user login

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,10 +1,16 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, UnauthorizedException } from "@nestjs/common";
 import { JwtService } from "@nestjs/jwt";
 import { User } from "@prisma/client";
+import { AuthLoginDTO } from "./domain/dto/authLogin.dto";
+import { PrismaService } from "../prisma/prisma.service";
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
-    constructor(private jwtService: JwtService) { }
+    constructor(
+        private readonly jwtService: JwtService, 
+        private readonly prisma: PrismaService,
+    ) { }
 
     async generateJwtToken(user: User) {
         const payload = { sub: user.id, name: user.name };
@@ -15,5 +21,15 @@ export class AuthService {
         };
 
         return {access_token: this.jwtService.sign(payload, options)}
+    }
+
+    async login({ email, password }: AuthLoginDTO) {
+        const user = await this.prisma.user.findUnique({where: {email}});
+
+        if (!user || !(await bcrypt.compare(password, user.password))) {
+            throw new UnauthorizedException('Email or password is incorrect');
+        }
+
+        return await this.generateJwtToken(user)
     }
 }
