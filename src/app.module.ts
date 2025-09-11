f7099993edf4eae66e45071220d0fa3f18b63910
EderSouza93 <ederaldo.souza93@gmail.com>
2025-09-10 22:09:34 -0300

    :sparkles: feat: send email to reset password

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { PrismaModule } from './modules/prisma/prisma.module';
 import { UserModule } from './modules/users/user.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { MailerModule } from '@nestjs-modules/mailer';
 
 @Module({
   imports: [
@@ -14,7 +15,13 @@ import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
         ttl: 5000,
         limit: 3,
       },
-    ])
+    ]),
+    MailerModule.forRoot({
+      transport: process.env.SMTP,
+      defaults: {
+        from: `"dnc_hotel" <${process.env.EMAIL_USER}>`
+      }
+    })
   ],
   providers: [
     {
--- src/modules/auth/domain/dto/authForgotPassword.dto.ts
+++ src/modules/auth/domain/dto/authForgotPassword.dto.ts
@@ -1,8 +1,8 @@
-import { IsEmail, IsNotEmpty, IsString } from "class-validator";
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class AuthForgotPasswordDTO {
-    @IsString()
-    @IsNotEmpty()
-    @IsEmail()
-    email: string;
+  @IsString()
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
 }
